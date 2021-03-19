c45477a53bcded30f56289ad3864b9ca41a1d382
Ketlyn Amora <ketypricila70@gmail.com>
2021-03-19 10:03:48 -0300

    inserido novos métodos

--- ExportExcelByProduto/Modelo/Produto.cs
+++ ExportExcelByProduto/Modelo/Produto.cs
@@ -7,14 +7,57 @@ namespace ExportExcelByProduto.Modelo
 {
     public class Produto
     {
-        /*
+
         private int _pedido;
         private string _dataCriacao;
         private string _loja;
         private string _canalVenda;
         private string _dataImportacao;
         private string _status;
-        */
+
+        public Produto(int pedido, string dataCriacao,
+            string loja, string canalVenda, string dataImportacao, string status)
+        {
+            this._pedido = pedido;
+            this._dataCriacao = dataCriacao;
+            this._loja = loja;
+            this._canalVenda = canalVenda;
+            this._dataImportacao = dataImportacao;
+            this._status = status;
+        }
+
+        public int Pedido 
+        { 
+            get => _pedido; 
+            set => _pedido = value; 
+        }
+        public string DataCriacao 
+        { 
+            get => _dataCriacao; 
+            set => _dataCriacao = value; 
+        }
+        public string Loja 
+        { 
+            get => _loja; 
+            set => _loja = value; 
+        }
+        public string CanalVenda 
+        { 
+            get => _canalVenda; 
+            set => _canalVenda = value; 
+        }
+        public string DataImportacao 
+        { 
+            get => _dataImportacao; 
+            set => _dataImportacao = value; 
+        }
+        public string Status 
+        { 
+            get => _status; 
+            set => _status = value; 
+        }
+
+        /*
         public Produto(int pedido, string dataCriacao,
             string loja, string canalVenda, string dataImportacao, string status)
         {
@@ -26,12 +69,14 @@ namespace ExportExcelByProduto.Modelo
             Status = status;
         }
 
-        public  int Pedido { get; set; }
-        public string DataCriacao { get; set; }
-        public string Loja { get; set; }
-        public string CanalVenda { get; set; }
-        public string DataImportacao { get; set; }
-        public string Status { get; set; }
+        public int Pedido { get; private set; }
+        public string DataCriacao { get; private set; }
+        public string Loja { get; private set; }
+        public string CanalVenda { get; private set; }
+        public string DataImportacao { get; private set; }
+        public string Status { get; private set; }
+
+        */
 
     }
 }
